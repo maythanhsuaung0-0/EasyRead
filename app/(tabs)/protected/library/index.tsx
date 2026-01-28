330aabb07b92fd2499481963c62ba065c2c3a959
maythanhsuaung0-0 <maythanhsuaung1111@gmail.com>
2026-01-28 17:49:27 -0500

    Revert "library page UI done"
    This reverts commit 9f90c01814c10ad7bae10c8d1bb7b42a993451dc.

--- app/(tabs)/protected/library/index.tsx
+++ app/(tabs)/protected/library/index.tsx
@@ -1,11 +1,10 @@
 import { useState, useEffect } from 'react';
 import { Button } from 'react-native-paper';
-import { View, Text, Image, Pressable, ScrollView } from 'react-native';
+import { View, Text, Image,ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import * as DocumentPicker from 'expo-document-picker';
 import * as FileSystem from 'expo-file-system/legacy';
 import PdfPageImage from 'react-native-pdf-page-image';
-import Ionicons from '@expo/vector-icons/Ionicons';
 import { useRouter } from 'expo-router';
 export default function LibraryScreen() {
   const [files, setFiles] = useState<string[]>([]);
@@ -50,25 +49,14 @@ export default function LibraryScreen() {
     const info = await FileSystem.getInfoAsync(thumbCachePath);
     if (info.exists) {
       return thumbCachePath;
+    } catch (e) {
+      console.error("Thumbnail failed", e);
+      return null; 
     }
-    if (fileName.endsWith('.pdf')) {
-      try {
-        const result = await PdfPageImage.generate(fullPdfPath, 0, 0.5);
-        // Move the result to our organized cache path
-        await FileSystem.moveAsync({
-          from: result.uri,
-          to: thumbCachePath
-        });
-        return thumbCachePath;
-      } catch (e) {
-        console.error("Thumbnail failed", e);
-        return null;
-      }
-    }
-
-    return null;
-  };
-
+  }
+  
+  return null; 
+};
   const getAllFiles = async () => {
     const scale = 1.0;
     const files = await FileSystem.readDirectoryAsync(FileSystem.documentDirectory || '');
@@ -78,11 +66,11 @@ export default function LibraryScreen() {
       if (thumbUri) {
         setFilePreviews(prev => ({ ...prev, [file]: thumbUri }));
       }
-      else {
+      else{
         setFilePreviews(prev => ({ ...prev, [file]: '' }));
-      }
-      setFiles(fileSelections);
     }
+    setFiles(fileSelections);
+  }
   }
   useEffect(() => {
     getAllFiles();
@@ -104,53 +92,41 @@ export default function LibraryScreen() {
     <SafeAreaView>
       <ScrollView>
         <View className="px-10 py-16 grid gap-2">
-          <View className="flex flex-row items-center justify-between">
-            <Text className="font-bold text-3xl ">Your Library</Text>
-            <View className="flex flex-row gap-3 items-center">
-              <Button mode="contained" onPress={() => {
-                pickDocument();
-              }} icon="cloud-upload">Upload Books</Button>
-            </View>
-          </View>
+          <Text className="font-bold text-3xl ">Your Library</Text>
           {/* Add your library content here */}
-          <View className="flex flex-col gap-4 mb-4 mt-6">
-            {files.length > 0 ? (
-              files.map((file, index) => (
-                <View key={index} className="flex flex-row gap-2">
-                  <Pressable className="my-1" onPress={onRead}>
+          {files.length > 0 ? (
+            files.map((file, index) => (
+              <Button key={index} mode="outlined" className="my-1" onPress={() => {
+                console.log('Selected file:', file);
+                const fileUri = FileSystem.documentDirectory + file;
+                console.log('File URI:', fileUri);
+                router.push(
+                  {
+                    pathname: '/library/[id]',
+                    params: { id: fileUri }
+                  }
+                )
+              }
+              }>
+              <Image
+                source={filePreviews[file] ? { uri: filePreviews[file] } : require('../../../assets/images/icon.png')}
+                className="w-12 h-16 mr-4"
+              />
+              <Text className="text-lg">{file}</Text>
 
-
-                    <Image
-                      source={filePreviews[file] ? { uri: filePreviews[file] } : require('../../../../assets/images/icon.png')}
-                      className="w-28 h-32 mr-4 rounded-sm self-center border border-gray-200 object-top"
-                    />
-                  </Pressable>
-                  <View className="flex-1  flex-col justify-between">
-                    <Text className="text-lg mt-4 self-start">{file}</Text>
-                    <View className="flex flex-row mt-auto mb-2">
-                      <Pressable className="p-3">
-                        <Ionicons name="book-outline" size={20} color="#000" />
-                      </Pressable>
-                      <Pressable className="p-3">
-                        <Ionicons name="trash-outline" size={20} color="#000" />
-                      </Pressable>
-                      <Pressable className="p-3"
-                      >
-                        <Ionicons name="heart-outline" size={20} color="#000" />
-                      </Pressable>
-                    </View>
-                  </View>
-                </View>
-
-              ))
-            ) :
-              (
-                <View className="">
-                  <Text className="text-lg text-gray-600">No items in your library yet.</Text>
-                </View>
-              )}
+              </Button>
+            ))
+          ) :
+            (
+              <View className="">
+                <Text className="text-lg text-gray-600">No items in your library yet.</Text>
+              </View>
+            )}
+          <View className="flex flex-row gap-3 items-center ">
+            <Button mode="contained" onPress={() => {
+              pickDocument();
+            }} icon="cloud-upload">Upload Books to the Library</Button>
           </View>
-
         </View>
       </ScrollView>
     </SafeAreaView>
